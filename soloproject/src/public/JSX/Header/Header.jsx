911bd54e511aafc2f587ffdf5f6c9c53d336c02b
ho <96030019+112park@users.noreply.github.com>
2024-09-02 14:56:14 +0900

    Add files via upload

---
+++ soloproject/src/public/JSX/Header/Header.jsx
@@ -0,0 +1,10 @@
+import '../../CSS/Header.css';
+
+export default function Header() {
+
+  return (
+    <div className='Header'>
+        <h2>Header</h2>
+    </div>
+  );
+}
---
+++ soloproject/src/public/JSX/Section/Section.jsx
@@ -0,0 +1,10 @@
+import '../../CSS/Section.css';
+
+export default function Section() {    
+
+  return (
+    <div className='Section'>
+        <button>section</button>
+    </div>
+  );
+}
